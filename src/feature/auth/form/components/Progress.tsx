7d0cba9ffac737437147514b5fdedb6389decdfc
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-03-13 13:49:27 +0700

    refactor: update font utility classes to use Tailwind's @apply and enhance Progress component styling

--- src/feature/auth/form/components/Progress.tsx
+++ src/feature/auth/form/components/Progress.tsx
@@ -3,7 +3,9 @@ import { Card, CardHeader } from "@/shared/component/ui/card";
 const Progress = () => {
   return (
     <Card className="px-12">
-      <CardHeader className="text-xl-bold">Langkah 1 dari 3</CardHeader>
+      <CardHeader>
+        <p className="font-xl-bold text-orange-900">Langkah 1 dari 3</p>
+      </CardHeader>
     </Card>
   );
 };
